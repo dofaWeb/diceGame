dffb6053eacd92575b0f4712510d272add7621e4
dofaWeb <156521486+dofaWeb@users.noreply.github.com>
2024-02-09 23:14:29 +0700

    Update script.js
    update rebirth, timeRoll

--- diceGame/script.js
+++ diceGame/script.js
@@ -7,14 +7,17 @@ let dicePrice = 25;
 let multiPrice = 100;
 let multiplier = 1;
 let timeRollPrice = 50;
-let rebirthMultiplier = 1;
+let rebirthMultiplier = 5;
 let rebirthPrice = 50000;
+let maxDice = 5;
 
 let buyButton = document.getElementsByClassName("buy");
 let description = document.getElementsByClassName("description");
 
 let timeRoll = 5000;
 const timeRollReuce = 500;
+
+let isTrigger = [false, false, false, false];
 
 let total = 0;
 
@@ -31,24 +34,51 @@ function rollDice() {
     const diceImg = document.getElementById("diceImg");
     currentSum = 0;
 
+    let hasCombo = [0, 0, 0, 0, 0, 0];
+    isTrigger = [false, false, false, false];
+
     const imgs = [];
 
     for (let i = 1; i <= numDice; i++) {
         //random the number 1-6
         const value = Math.floor(Math.random() * 6) + 1;
+        hasCombo[value - 1]++;
         imgs.push(`<img src="img/Dice${value}.png" alt="Dice ${value}" width="100">`);
         currentSum += value * multiplier * rebirthMultiplier;
+    }
+    for(let i = 0; i < 6; i++) {
+        if(hasCombo[i] >= 5) {
+            isTrigger[3] = true;
+            hasCombo[i] -= 5;
+            currentSum *= 5;
+        }
+        if(hasCombo[i] >= 4) {
+            isTrigger[2] = true;
+            hasCombo[i] -= 4;
+            currentSum *= 4;
+        }
+        if(hasCombo[i] >= 3) {
+            isTrigger[1] = true;
+            hasCombo[i] -= 3;
+            currentSum *= 3;
+        }
+        if(hasCombo[i] >= 2) {
+            isTrigger[0] = true;
+            hasCombo[i] -= 2;
+            currentSum *= 2;
+        }
+
     }
     diceImg.innerHTML = imgs.join(" ");
     sum += currentSum;
     total += currentSum;
-    
+
 }
 
 //update the timer
-function updateTime(){
+function updateTime() {
     timer++;
-    if(timer >= timeRoll/10){
+    if (timer >= timeRoll / 10) {
         rollDice();
         timer = 1;
     }
@@ -60,8 +90,22 @@ function updateTime(){
 //update the display
 function update() {
     diceResult.textContent = `Money: $${sum} | + $${currentSum}`;
-    
-    numDiceDisplay.textContent = "Dice: " + numDice;
+    switch(isTrigger.indexOf(true)) {
+        case 0:
+            diceResult.textContent += " | Double";
+            break;
+        case 1:
+            diceResult.textContent += " | Triple";
+            break;
+        case 2:
+            diceResult.textContent += " | Quad";
+            break;
+        case 3:
+            diceResult.textContent += " | Penta";
+            break;
+    }
+
+    numDiceDisplay.textContent = "Dice: " + numDice + "/" + maxDice;
     multiplierDisplay.textContent = "Multiplier: " + multiplier;
     totalDisplay.textContent = "Total: $" + total;
     timeRollDisplay.textContent = "Time Roll: " + (timeRoll / 1000).toFixed(1) + "s";
@@ -72,12 +116,18 @@ function update() {
     description[1].textContent = `$${multiPrice}`;
     description[2].textContent = `$${timeRollPrice}`;
     description[3].textContent = `$${rebirthPrice}`;
-    
+
+    if(rebirthMultiplier >= 5 && timeRoll <= 500) {
+        buyButton[2].textContent = "Time Roll - 0.1s";
+    }else {
+        buyButton[2].textContent = "Time Roll - 0.5s";
+    }
+
 }
 
 //add 1 dice
 buyButton[0].addEventListener("click", function () {
-    if (sum >= dicePrice) {
+    if (sum >= dicePrice && numDice < maxDice) {
         sum -= dicePrice;
         numDice++;
         dicePrice += 50 * numDice;
@@ -97,24 +147,39 @@ buyButton[1].addEventListener("click", function () {
 
 //reduce timeRoll by 0.5s
 buyButton[2].addEventListener("click", function () {
-    if (sum >= timeRollPrice && timeRoll > 500) {
-        sum -= timeRollPrice;
-        timeRoll -= timeRollReuce;
-        timeRollPrice += 100;
-        update();
+    if (rebirthMultiplier <= 5) {
+        if (sum >= timeRollPrice && timeRoll > 500) {
+            sum -= timeRollPrice;
+            timeRoll -= timeRollReuce;
+            timeRollPrice += 100;
+            update();
+        }
+    } else {
+        if (sum >= timeRollPrice && timeRoll > 100 && timeRoll <= 500) {
+            sum -= timeRollPrice;
+            timeRoll -= 100;
+            timeRollPrice += 1000;
+            update();
+        }else if(sum >= timeRollPrice && timeRoll > 500){
+            sum -= timeRollPrice;
+            timeRoll -= timeRollReuce;
+            timeRollPrice += 100;
+            update();
+        }
     }
 });
 
 //rebirth
 buyButton[3].addEventListener("click", function () {
     if (sum >= rebirthPrice) {
+        maxDice+=2;
         sum = 0;
         diceImg.innerHTML = "";
         currentSum = 0;
         rebirthMultiplier++;
         rebirthPrice *= 3;
         numDice = 1;
-        dicePrice = 25 *rebirthMultiplier;
+        dicePrice = 25 * rebirthMultiplier;
         multiplier = 1;
         multiPrice = 100 * rebirthMultiplier;
         timeRoll = 5000;
